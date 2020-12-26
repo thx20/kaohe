9034d72a55c9ac66324ab9b7dcf83517abd6258c
thx20 <74035754+thx20@users.noreply.github.com>
2020-12-26 16:08:00 +0800

    Add files via upload

---
+++ xipai.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	//黑桃
+	Spade = 0
+	//红桃
+	Hearts = 1
+	//梅花
+	Club = 2
+	//方块
+	Diamond = 3
+)
+
+type Poker struct {
+	Num int
+	Flower int
+}
+
+func (p Poker)PokerSelf()string  {
+	var buffer string
+
+	switch p.Flower {
+	case Spade:
+		buffer += "♤"
+	case Hearts:
+		buffer += "♡"
+	case Club:
+		buffer += "♧"
+	case Diamond:
+		buffer += "♢"
+	}
+	switch p.Num {
+	case 13:
+		buffer += "2"
+	case 12:
+		buffer += "A"
+	case 11:
+		buffer += "K"
+	case 10:
+		buffer += "Q"
+	case 9:
+		buffer += "J"
+	default:
+		buffer += strconv.Itoa(p.Num+2)
+	}
+
+	return buffer
+}
+
+func CreatePokers()(pokers Pokers)  {
+	for i := 1; i < 14; i++ {
+		for j := 0; j < 4; j++ {
+			pokers = append(pokers,Poker{
+				Num:    i,
+				Flower: j,
+			})
+		}
+	}
+	return
+}
+
+type Pokers []Poker
+
+
+
+func (p Pokers)Print()  {
+	for _, i2 := range p {
+		fmt.Print(i2.PrintPoker()," ")
+	}
+	fmt.Println()
+}
+
+func main(){
+var pai Pokers
+var pok [...]Pokers{CreatePokers()}
+fmt.Println("pok")
+
+}
+
